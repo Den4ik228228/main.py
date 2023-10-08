0e80cb2d935df12f0cbf6b40be084a45dec96b30
Den4ik228228 <130787032+Den4ik228228@users.noreply.github.com>
2023-10-08 11:42:40 +0300

    New Discord update

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="$", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print("Я готов!")
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Привет, человек!")
+
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send("Пока, человек!")
+
+
+@bot.command()
+async def repeat(ctx, times: int, content = "repeating..."):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+
+bot.run("MTE1NTU0OTQxNzI0MDI3NzAzMg.G2m02z.iUxyZw0Ng0G_dtaMBPqVLoFktyWg_oP94K5DPU")
 import time
 print('Здравствуй, здесь переводчик со сленгового языка, наслаждайся)')
 while True:
